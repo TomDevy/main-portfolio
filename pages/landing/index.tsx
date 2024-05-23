24ceffad486cacdb537158722f975e60f4f6ab37
TomDevy <agboolaemmanuel524@gmail.com>
2024-05-23 08:56:26 +0100

    begin landing page

--- pages/landing/index.tsx
+++ pages/landing/index.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link';
 import styles from '../../styles/home.module.scss';
+import { BtnSecondary } from '../../components/button';
 const Landing = () => {
     return (
         <div className={styles.container}>
-          <div className={styles.nav}></div>
+          <div className={styles.nav}>
+            <BtnSecondary>Start a project</BtnSecondary>
+          </div>
     </div>
     )
 }
