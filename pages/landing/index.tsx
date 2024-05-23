8888c87da704a7a066801ca4d6cdbd5b63696f85
TomDevy <agboolaemmanuel524@gmail.com>
2024-05-23 09:33:49 +0100

    begin landing page

--- pages/landing/index.tsx
+++ pages/landing/index.tsx
@@ -1,14 +1,33 @@
-import Link from 'next/link';
-import styles from '../../styles/home.module.scss';
-import { BtnSecondary } from '../../components/button';
+import Link from "next/link";
+import styles from "../../styles/home.module.scss";
+import { BtnSecondary } from "../../components/button";
 const Landing = () => {
-    return (
-        <div className={styles.container}>
-          <div className={styles.nav}>
-            <BtnSecondary>Start a project</BtnSecondary>
+  return (
+    <div className={styles.container}>
+      <div className={styles.nav}>
+        <BtnSecondary>Start a project</BtnSecondary>
+      </div>
+      <div className={styles.heroContainer}>
+        <div className={styles.heroContent}>
+          <h1>Why work with me?</h1>
+          <div className={styles.workFlex}>
+            <div className={styles.workCard}>
+              <h3>Experience</h3>
+            </div>
+            <div className={styles.workCard}>
+              <h3>Experience</h3>
+            </div>
+            <div className={styles.workCard}>
+              <h3>Experience</h3>
+            </div>
+            <div className={styles.workCard}>
+              <h3>Experience</h3>
+            </div>
           </div>
+        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Landing
+export default Landing;
