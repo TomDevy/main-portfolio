7525cedf588e477b421db9d66add67e98a48037e
TomDevy <agboolaemmanuel524@gmail.com>
2024-05-24 08:16:12 +0100

    create review section

--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -5,6 +5,20 @@ const Review = () => {
             <div className={style.review}>
                 <div></div>
             </div>
+            <div className={style.stats}>
+                <div className={style.eachStats}>
+                    <h4>Projects Created</h4>
+                    <h1>6+</h1>
+                </div>
+                <div className={style.eachStats}>
+                    <h4>Years of Experience</h4>
+                    <h1>4 Years</h1>
+                </div>
+                <div className={style.eachStats}>
+                    <h4>Client Satisfaction</h4>
+                    <h1>98%</h1>
+                </div>
+            </div>
         </div>
     )
 }
